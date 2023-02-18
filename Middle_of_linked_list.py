8a23fb83be479a9a9615d346306730ef08d161a6
RAGHAVJIMZN <raghavdiwakar17@gmail.com>
2023-02-18 20:45:09 +0530

    .
    .

--- Middle_of_linked_list.py
+++ Middle_of_linked_list.py
@@ -3,11 +3,11 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-# class Solution:
-#     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-#         first = second = head
-#         while second and second.next:
-#             first = first.next
-#             second = second.next.next
-#         return first
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        first = second = head
+        while second and second.next:
+            first = first.next
+            second = second.next.next
+        return first
         
