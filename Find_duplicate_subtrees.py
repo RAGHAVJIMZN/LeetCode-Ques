58ca90b62f4ed9a785634e3b9b485c216bdd075b
RAGHAVJIMZN <raghavdiwakar17@gmail.com>
2023-02-28 15:35:40 +0530

    Create Find_duplicate_subtrees.py

---
+++ Find_duplicate_subtrees.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+        def postOrderTraversal(node, lookup, result):
+            if not node:
+                return ""
+            s = "(" + postOrderTraversal(node.left, lookup, result) + \
+                str(node.val) + \
+                postOrderTraversal(node.right, lookup, result) + \
+                ")"
+            if lookup[s] == 1:
+                result.append(node)
+            lookup[s] += 1
+            return s
+
+        lookup = collections.defaultdict(int)
+        result = []
+        postOrderTraversal(root, lookup, result)
+        return result
