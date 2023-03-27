5b76eda6a7608685fabf808d94b44a2141ab3a8a
RAGHAVJIMZN <raghavdiwakar17@gmail.com>
2023-03-27 09:30:51 +0530

    Update Minimum_path_sum.py

--- Minimum_path_sum.py
+++ Minimum_path_sum.py
@@ -7,11 +7,11 @@ class Solution:
         for i in range(1, cols):
             grid[0][i] += grid[0][i-1]
         
-        # first col
-        for i in range(1, rows):
-            grid[i][0] += grid[i-1][0]
-        # inner cells
-        for i in range(1, rows):
-            for j in range(1, cols):
-                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
-        return grid[-1][-1]
+        # # first col
+        # for i in range(1, rows):
+        #     grid[i][0] += grid[i-1][0]
+        # # inner cells
+        # for i in range(1, rows):
+        #     for j in range(1, cols):
+        #         grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+        # return grid[-1][-1]
