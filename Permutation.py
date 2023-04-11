ca34aeff1ac0f6aeb905c6e3a949d6a66c38fc23
RAGHAVJIMZN <raghavdiwakar17@gmail.com>
2023-04-11 16:55:20 +0530

    /

--- Permutation.py
+++ Permutation.py
@@ -4,21 +4,21 @@ class Solution:
         
         visited = set()
         self.helper(results, nums, [], 0, visited)
-        
+            
         return results
     
     def helper(self, results, nums, combination, start, visited):
         if len(combination) == len(nums):
             results.append(list(combination))
             return
-        
-        
+           
         for i in range(len(nums)):
             if nums[i] in visited:
                 continue
-            
+    
             visited.add(nums[i])
             combination.append(nums[i])
             self.helper(results, nums, combination, i + 1, visited)
             combination.pop()            
             visited.remove(nums[i])
+            
---
+++ RemovingStarsFromString.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        n = s.count('*')
+        for i in range(n):
+            ind = s.find('*')
+            s = s[0:ind-1] + s[ind+1:]
+        return s 
